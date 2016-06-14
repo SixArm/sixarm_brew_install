61e9759f65709a9c508b4c13f098dffa8ff21cca
Joel Parker Henderson <joel@joelparkerhenderson.com>
2016-06-14 17:07:43 -0400

    Add Swift testing

--- brew-install-our-swift.sh
+++ brew-install-our-swift.sh
@@ -2,3 +2,12 @@
 
 # Tool to help with Swift style and conventions.
 brew install swiftlint
+
+# Appium test automation framework
+brew cask install appium
+
+# Taylor is a Swift code quality metrics tool.
+brew install taylor
+
+# SourceKitten attaches to SourceKit AST
+brew install sourcekitten
