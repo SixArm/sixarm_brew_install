a017aa62702e0c662003251d7bdf800dff24acdd
Joel Parker Henderson <joel@joelparkerhenderson.com>
2016-06-26 16:57:08 -0400

    Organize databases into tech

--- brew-install-our-tech.sh
+++ brew-install-our-tech.sh
@@ -45,6 +45,59 @@ brew cask install realm-browser
 brew cask install tunnelblick
 
 ##
+# Databases
+#
+# This section installs many databases and database tooling:
+# Cassandra, CouchDB, Hadoop, MariaDB, MongoDB, PostgreSQL,
+# RabbitMQ, Redis, Riak, Sphinx, SQLite. Notably *not* MySQL.
+##
+
+## Database-Related
+
+# Cassandra database.
+brew install cassandra
+
+# CouchDB database, esp. for document-oriented storage.
+brew install couchdb
+
+# Hadoop database.
+brew install hadoop
+
+# MariaDB database
+brew install mariadb
+
+# MongoDB database.
+brew install mongodb
+
+# PostgreSQL database.
+brew install postgresql
+
+# RabbitMQ enterprise message queue based on the emerging AMQP standard.
+brew install rabbitmq
+
+# Redis database, esp. for key-value cache and store, and data structures.
+brew install redis
+
+# Riak open-source distributed database.
+brew install riak
+
+# SQLite database: self-contained, serverless, zero-configuration, transactional engine.
+brew install sqlite && brew link sqlite
+
+## Database searchers
+
+# Sphinx search engine.
+brew install mysql && brew install postgresql && brew install sphinx
+
+## Database managers
+
+# Toad database manager by Dell
+brew cask install toad
+
+# Valentina Studio database manager
+brew cask install valentina-studio
+
+##
 # Markup Tooling
 #
 # This section installs markup tooling.
