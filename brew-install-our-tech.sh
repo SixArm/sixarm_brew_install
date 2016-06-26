d2d8aa7ba4c663dd3683dc77402f714ce3efda5a
Joel Parker Henderson <joel@joelparkerhenderson.com>
2016-06-26 16:52:25 -0400

    Organize programming languages into tech

--- brew-install-our-tech.sh
+++ brew-install-our-tech.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# brew-install-our-tech-packages-automatically.sh
+# brew-install-our-tech.sh
 #
 # Use Homebrew to install our favorite tech-related packages
 # that can be installed fully automatically i.e. unattended;
@@ -12,7 +12,7 @@
 # an issue, or emailing us, or creating a pull request. Thanks!
 ##
 
-## Environment-Related
+## Environment
 
 # Code Climate Platform for all static analysic
 brew tap codeclimate/formulas && brew install codeclimate
@@ -38,15 +38,131 @@ brew install maven
 
 ## Mac programming
 
-# Alcatraz Xcode plugin manager
-brew cask install alcatraz
-
 # Realm browser for the Realm embedded database
 brew cask install realm-browser
 
 # Tunnelblick remote access VPN
 brew cask install tunnelblick
 
-## Cleanup - this is always the last thing to do
+##
+# Markup Tooling
+#
+# This section installs markup tooling.
+# For example this section is a good place for HTML tools,
+# Markdown tools, UML tools, XML tools, and similar.
+##
 
-brew cleanup
+## Markdown
+
+# MacDown simple markdown editor
+brew cask install macdown
+
+## UML
+
+# StarUML modeling tool
+brew cask install staruml
+
+##
+# Programming Languages
+#
+# This section installs many programming languages:
+# Clojure, Elixir, Erlang, Go, Haskell, JavaScript,
+# Perl, Python, R, Ruby, Scala, Swift, and tooling.
+##
+
+## Clojure
+
+# Clojure programming language compiler.
+brew install clojure-compiler
+
+# Leiningen automates Clojure projects.
+brew install leiningen
+
+## Elixir
+
+# Elixir programming language built on top of the Erlang VM.
+brew install elixir
+
+## Erlang
+
+# Erlang programming language for scalable high-availability systems.
+brew install erlang
+
+## Go
+
+# Go programming language by Google; compare `C`.
+brew install go
+
+## Haskell
+
+# Cabal is a package manager for Haskell
+brew install ghc cabal-install
+
+## JavaScript
+
+# Node.js is a JavaScript platform for building fast, scalable network app.
+brew install node
+
+# PhantomJS is a headless WebKit scriptable with a JavaScript API.
+brew install phantomjs
+
+# V8 JavaScript Engine.
+brew install v8
+
+## Perl
+
+# Perl programming language, esp. for systems administration.
+brew install perl
+
+# Perl-Compatible Regular Expressions pattern matching tools.
+brew install pcre
+
+# CPAN search for perl modules
+brew install cpansearch
+
+## Python
+
+# Python programming language, esp. for systems scripting.
+brew install python
+brew install python3
+
+## R
+
+# R programming language, esp. for statistics.
+brew install r
+
+## Ruby
+
+# chruby changes the current Ruby.
+brew install chruby
+
+# JRuby is a high performance, stable, fully threaded Java implementation of Ruby.
+brew install jruby
+
+# Ruby programming language; compare `perl`, `python`.
+brew install ruby
+
+# Tool to install various implementations of Ruby.
+brew install ruby-install
+
+## Scala
+
+# Scala programming language, that runs on top of the JVM.
+brew install scala
+
+## Swift
+
+# Tool to help with Swift style and conventions.
+brew install swiftlint
+
+# Appium test automation framework
+brew cask install appium
+
+# Taylor is a Swift code quality metrics tool.
+brew install taylor
+
+# SourceKitten attaches to SourceKit AST.
+brew install sourcekitten
+
+# Alcatraz Xcode plugin manager
+brew cask install alcatraz
